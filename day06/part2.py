89149736373173bbd2668ffd2d5b8698193beb20
Markus Warne <markus.warne@gmail.com>
2020-12-06 20:37:54 +0100

    clean up day 6

--- day06/part2.py
+++ day06/part2.py
@@ -1,11 +1,11 @@
 
+def count_answers(group):
+  grp_str = ''.join(group)
+  # for unique answers in each group, count if occours as many times as members
+  return len([u for u in set(grp_str) if grp_str.count(u) == len(group)])
+
 with open('input.txt', 'r') as file:
   groups = [g.split('\n') for g in file.read().split('\n\n')]
-  count = 0
+  answer_count = sum(map(count_answers, groups))
 
-  for g in groups:
-    grp_answers_str = ''.join(g)
-    unique = set(grp_answers_str)
-    count += len([u for u in unique if grp_answers_str.count(u) == len(g)])
-
-  print(count)
+  print(answer_count)
