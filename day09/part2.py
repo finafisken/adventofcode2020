43708535fb56d2973eacc2c50dec37da8bd95e4d
Markus Warne <markus.warne@gmail.com>
2020-12-09 18:44:13 +0100

    cleanup pt2

--- day09/part2.py
+++ day09/part2.py
@@ -4,14 +4,15 @@ file = open('input.txt', 'r')
 numbers = [int(n) for n in file.read().split('\n')]
 
 target = 776203571 # part 1 result
-
 found = False
-for start_i, n in enumerate(numbers):
-  if found:
-    break
-  for end_i, n in enumerate(numbers, start_i):
+
+for start_i in range(0, len(numbers)):
+  for end_i in range(start_i, len(numbers)):
     sequence = numbers[start_i:end_i]
     if sum(sequence) == target:
       print(min(sequence) + max(sequence))
       found = True
       break
+
+  if found:
+    break
