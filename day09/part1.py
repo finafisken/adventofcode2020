02fd5e572e9d84293f19a2c67d14677f0d1d1138
Markus Warne <markus.warne@gmail.com>
2020-12-09 18:46:14 +0100

    formatting

--- day09/part1.py
+++ day09/part1.py
@@ -1,12 +1,13 @@
 from itertools import combinations
 
-file = open('input.txt', 'r')
-numbers = [int(n) for n in file.read().split('\n')]
+with open('input.txt', 'r') as file:
+  numbers = [int(n) for n in file.read().split('\n')]
+  preamble = 25
 
-preamble = 25
-for i in range(preamble, len(numbers)):
-  preamble_combos = list(combinations(numbers[i-preamble:i], 2))
-  preamble_combos_sum = [a + b for (a, b) in preamble_combos]
-  if i < len(numbers) and numbers[i] not in preamble_combos_sum:
-    print(numbers[i])
-    break
+  for i in range(preamble, len(numbers)):
+    preamble_combos = list(combinations(numbers[i-preamble:i], 2))
+    preamble_combos_sum = [a + b for (a, b) in preamble_combos]
+
+    if i < len(numbers) and numbers[i] not in preamble_combos_sum:
+      print(numbers[i])
+      break
--- day09/part2.py
+++ day09/part2.py
@@ -1,18 +1,16 @@
-from itertools import combinations
+with open('input.txt', 'r') as file:
+  numbers = [int(n) for n in file.read().split('\n')]
 
-file = open('input.txt', 'r')
-numbers = [int(n) for n in file.read().split('\n')]
+  target = 776203571 # part 1 result
+  found = False
 
-target = 776203571 # part 1 result
-found = False
+  for start_i in range(0, len(numbers)):
+    for end_i in range(start_i, len(numbers)):
+      sequence = numbers[start_i:end_i]
+      if sum(sequence) == target:
+        print(min(sequence) + max(sequence))
+        found = True
+        break
 
-for start_i in range(0, len(numbers)):
-  for end_i in range(start_i, len(numbers)):
-    sequence = numbers[start_i:end_i]
-    if sum(sequence) == target:
-      print(min(sequence) + max(sequence))
-      found = True
+    if found:
       break
-
-  if found:
-    break
